708aa86a7b094275ab5255bc64cde13fc18778fb
Fast Ninja <damasceno.developer21@gmail.com>
2023-10-27 23:44:54 -0300

    finished project with tests on code pen passing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,10 @@ interface DataYearObjectArray {
       temperature: number
     }[]
   }
-
   interface ToolTipPositionProps {
     top: number;
     left: number;
   }
-
 
 export default function Home() {
 
@@ -43,11 +41,7 @@ export default function Home() {
 
   let xScale: d3.ScaleLinear<number, number, never> = d3.scaleLinear();
 
-  // if (result === undefined) {
-  //   return;
-  // }  
-  const monthsArray = ["January","February","March","April","May","June","July",
-  "August","September","October","November","December"];
+  const monthsArray = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   
   const [minYear, maxYear] = d3.extent(result.monthlyVariance.map(x => x.year))  
 
@@ -79,8 +73,8 @@ export default function Home() {
       setSelectedElement({year: elementYear.year, ...elementMonthData});
     }
 
-    const clientX = e.clientX;
-    const clientY = e.clientY;
+    const clientX = e.pageX;
+    const clientY = e.pageY;
     const toolTipPadding = -100;
 
     setTooltipPosition({
@@ -94,36 +88,31 @@ export default function Home() {
   function handleMouseLeave() {
     setToolTipAttrs('opacity-0')
   }
+  
+  const colorRanges = [
+    { min: -99, max: 2.8, color: "rgb(21, 81, 161)" },
+    { min: 2.8, max: 3.9, color: "rgb(69, 117, 180)" },
+    { min: 3.9, max: 5, color: "rgb(116, 173, 209)" },
+    { min: 5, max: 6.1, color: "rgb(171, 217, 233)" },
+    { min: 6.1, max: 7.2, color: "rgb(224, 243, 248)" },
+    { min: 7.2, max: 8.3, color: "rgb(255, 255, 191)" },
+    { min: 8.3, max: 9.5, color: "rgb(254, 224, 144)" },
+    { min: 9.5, max: 10.6, color: "rgb(253, 174, 97)" },
+    { min: 10.6, max: 11.7, color: "rgb(244, 109, 67)" },
+    { min: 11.7, max: 12.8, color: "rgb(215, 48, 39)" },
+    { min: 12.8, max: 99, color: "rgb(154, 11, 4)" },
+  ];
 
-  function tempColor(temp: number) : string {
-    if (temp < 2.8) {
-      return "rgb(21, 81, 161)"
-    }
-    else if(temp >= 2.8 && temp < 3.9) {
-      return "rgb(69, 117, 180)";
-    } else if (temp >= 2.8 && temp < 5) {
-      return "rgb(116, 173, 209)";
-    }else if (temp >= 5 && temp < 6.1) {
-      return "rgb(171, 217, 233)";
-    }else if (temp >= 6.1 && temp < 7.2) {
-      return "rgb(224, 243, 248)";
-    }else if (temp >= 7.2 && temp < 8.3) {
-      return "rgb(255, 255, 191)";
-    }else if (temp >= 8.3 && temp < 9.5) {
-      return "rgb(254, 224, 144)";
-    }else if (temp >= 9.5 && temp < 10.6) {
-      return "rgb(253, 174, 97)";
-    }else if (temp >= 10.6 && temp < 11.7) {
-      return "rgb(244, 109, 67)";
-    }else if (temp >= 11.7 && temp < 12.8) {
-      return "rgb(215, 48, 39)";
-    } else if (temp >= 12.8) {
-      return "rgb(154, 11, 4)";
+  function tempColor(temp: number): string {  
+    const matchedRange = colorRanges.find(range => temp >= range.min && temp < range.max);
+  
+    if (matchedRange) {
+      return matchedRange.color;
     } else {
-      throw new Error(`Temperature ${temp} is not in the range of valid values`)
+      throw new Error(`Temperature ${temp} is not in the range of valid values`);
     }
   }
-
+  
   const svgHeight = 540
   const svgWidth = 1603
   const padding = {
@@ -143,8 +132,8 @@ export default function Home() {
   let startXcoordinate = padding.Left - rectWidth;
   let startYcoordinate = padding.Top - rectHeight;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="wrapper flex flex-col items-center ">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 overflow-x-hidden">
+      <div className="wrapper flex flex-col items-center">
         <h1 id='title' className="bg-amber-50 text-black w-full text-center text-3xl pt-4 font-bold">Monthly Global Land-Surface Temperature</h1>
         <h2 id='description' className="bg-amber-50 text-black w-full text-center text-xl pt-4 mb-0 pb-0">{minYear} - {maxYear}: base temperature {result.baseTemperature}°C</h2>
         <ToolTip
@@ -167,7 +156,7 @@ export default function Home() {
                         x={startXcoordinate} 
                         y={startYcoordinate}
                         className="cell hover:stroke-black"
-                        data-month={monthData.month}
+                        data-month={monthData.month - 1}
                         data-year={data.year}
                         data-temp={monthData.temperature}
                         onMouseEnter={(event) => {handleMouseEnter(event, data.year, monthData.month)}}
@@ -194,9 +183,162 @@ export default function Home() {
               color='black'
             />
 
+            <Legend
+              colors={colorRanges}
+              Xposition={600}
+              Yposition={550}
+              svgWidth={svgWidth}
+              textColor='black'
+            />
+
         </svg>
       </div>
     </main>
+  )
+}
+interface ColorProperty {
+  min: number, max: number, color: string;
+}
+
+interface LegendProps {
+  colors: ColorProperty[]
+  Yposition: number;
+  Xposition: number;
+  svgWidth: number;
+  textColor: string;
+}
+
+function Legend({colors, Yposition, Xposition, textColor} : LegendProps) {
+  
+  const rectWidth = 36; const rectHeight = 27;
+  const rangeOfMinTemperatures = colors.map(t => t.min);
+  const rangeOfMaxTemperatures = colors.map(t=> t.max);
+  const [minTemp, maxTemp] = d3.extent([...rangeOfMinTemperatures, ...rangeOfMaxTemperatures]) as [number, number]
+  const rangeOfValues = rangeOfMinTemperatures.filter(t=> t !== minTemp)
+
+  const [element, setElement] = useState<ColorProperty>({min: 0, max: 0, color: ''})
+  const [tooltipPosition, setTooltipPosition] = useState<ToolTipPositionProps>({ top: 0, left: 0 });
+  const [tooltipAttrs, setToolTipAttrs] = useState('opacity-0 transition-all duration-200');
+  const [foreignObjectAttrs, setForeignObjectAttrs] = useState('hidden');
+
+  function handleMouseEnter(e: React.MouseEvent<SVGRectElement, MouseEvent>, index: number) {
+
+    const element = colors[index];
+        
+    if(element) {
+      setElement(element);
+    }
+
+    const clientX = e.screenX;
+    const clientY = e.pageY;
+    const toolTipYadjust = -450;
+    const toolTipXadjust = -750
+    
+    setTooltipPosition({
+      top: clientY + toolTipYadjust, 
+      left: clientX + toolTipXadjust,
+    });
+    setToolTipAttrs('opacity-80')
+    setForeignObjectAttrs('flex')
+    
+  }
+
+  function handleMouseLeave() {
+    setToolTipAttrs('opacity-0')
+    setForeignObjectAttrs('hidden')
+  }
+
+  function LegendToolTipText({element, minTemp, maxTemp} : {element: ColorProperty, minTemp : number, maxTemp: number }) {
+    if (element.min === minTemp) {
+      return (
+        <>
+          <p>Temperatures lower</p>
+          <p>than {element.max}°C</p>
+        </>
+      ) 
+    } else if (element.max === maxTemp) {
+      return (
+        <>
+          <p>Temperatures greater </p>
+          <p>than {element.min}°C</p>
+        </>
+      ) 
+    } else {
+      return (
+        <>
+          <p>Temperatures between</p>
+          <p>{element.min}°C and {element.max}°C</p>
+        </>
+      )
+    }
+  }
+  
+  return (
+    <>
+      <foreignObject className={`relative ${foreignObjectAttrs}`} x='550' y='255' width={colors.length*rectWidth + 200} height={250}>
+        <div id='LengedTooltip' 
+        className={`flex flex-col justify-center items-center bg-slate-600 text-white p-2 absolute rounded select-none pointer-events-none transition-opacity ${tooltipAttrs}`}
+        style={{ top: tooltipPosition.top + 'px', left: tooltipPosition.left + 'px' }}
+        >
+        {element && <LegendToolTipText element={element} minTemp={minTemp} maxTemp={maxTemp}/>}
+        </div>
+      </foreignObject>
+      <g id='legend'>
+          <path
+          // d="M x1 y1 H x2 y2"
+          d={`M ${Xposition - 13} ${Yposition  - 40} H ${Xposition + (colors.length)*rectWidth}`}
+          stroke="currentColor"
+          strokeWidth={1}
+          />                
+          <text
+          x={Xposition-95}
+          y={Yposition - 55}
+          fill={textColor}
+          style={{
+            fontSize: "10px",
+            fontWeight: 'bold',
+            textAnchor: "middle",
+            transform: "translateY(20px)"
+          }}> Legend: Range of temperatures
+        </text>
+        {rangeOfValues.map((value,index) => (
+                <g
+                className='tick'
+                key={index}
+                transform={`translate(${Xposition - 7 + rectWidth*(index + 1)}, ${Yposition -40})`}
+                >
+                  <line
+                    y2="6"
+                    stroke="currentColor"
+                  />
+                  <text
+                    key={index}
+                    fill={textColor}
+                    style={{
+                      fontSize: "10px",
+                      textAnchor: "middle",
+                      transform: "translateY(20px)"
+                    }}>
+                    { value }
+                  </text>
+                </g>
+        ))}
+        {colors.map((value,index) => (
+          
+          <rect
+            key={index}
+            x={Xposition -8 + rectWidth*index}
+            y={Yposition - 68}
+            width={rectWidth}
+            height={rectHeight}
+            fill={value.color}
+            className="hover:stroke-black"
+            onMouseEnter={(event) => {handleMouseEnter(event, index)}}
+            onMouseLeave={handleMouseLeave}
+          ></rect>
+        ))}
+      </g>
+    </>
   )
 }
 
@@ -222,10 +364,9 @@ function ToolTip({monthsArray, element, position, toolTipAttrs}: ToolTipProps) {
         <p>{element.variance > 0 && '+'}{element.variance}°C</p> 
       </>
     )}   
-</div>
+    </div>
   )
 }
-
 
 interface AxisProperties {
   svgHeight: number;  
@@ -284,7 +425,6 @@ interface AxisLeftProperties extends AxisProperties {
   rectHeight: number;
 }
 
-
 function AxisLeft({monthsArray, rectHeight, padding, svgHeight, color}: AxisLeftProperties) {
 
   return (
@@ -321,7 +461,6 @@ function AxisLeft({monthsArray, rectHeight, padding, svgHeight, color}: AxisLeft
       </g>
   )
 }
-
 
 function useData(url: string) {
   const [data, setData] = useState<Data>({baseTemperature: 8.66, monthlyVariance: [{year: 1753, month: 1, variance: -1.366}]});
